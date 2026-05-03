0082e44cdd1be34f8ec7168a977bf4fda4f359f2
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-05-02 19:26:39 -0700

    Basic GET endpoints

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const http = require('http');
+const { Hono } = require('hono');
+const { serve } = require('@hono/node-server');
 
-const serve = async (port, hostname = '127.0.0.1') => {
-  const server = http.createServer(async (request, response) => {
-    const { url } = request;
-    if (url === '/' || url === '/index.html') {
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end('<h1>Hello, Node.js!</h1>');
-    } else if (url === '/health') {
-      response.writeHead(200).end(`OK ${Date.now()}`);
-    } else {
-      console.error(`${url} is 404!`);
-      response.writeHead(404);
-      response.end();
-    }
-  });
+const app = new Hono();
 
-  server.listen(port);
-  console.log('Listening on', hostname, 'port', port);
-};
+app.get('/', (c) => c.text('Hello from Hono!'));
 
-(async () => {
-  const port = process.env.PORT || 3000;
-  await serve(port);
-})();
+app.get('/health', (c) => c.text(`OK ${Date.now()}`));
+
+const port = process.env.PORT || 3000;
+serve({ fetch: app.fetch, port });
+console.log('Listening on port', port);
